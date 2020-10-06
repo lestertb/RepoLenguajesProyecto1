3af2b63cfd0dd8e5b1444f247a02ab8a78f18b87
lester <lestertb@estudiantec.cr>
2020-10-05 21:30:19 -0600

    GettingStarted
    Inicio del Proyecto

---
+++ Proyecto1Lenguajes/cmake-build-debug/CMakeFiles/Proyecto1Lenguajes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1Lenguajes.dir/main.c.obj"
+  "Proyecto1Lenguajes.exe"
+  "Proyecto1Lenguajes.exe.manifest"
+  "Proyecto1Lenguajes.pdb"
+  "libProyecto1Lenguajes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Proyecto1Lenguajes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
