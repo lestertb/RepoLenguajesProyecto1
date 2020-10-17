ca5992d7c4124a4341d18807014792acf1710850
lester <lestertb@estudiantec.cr>
2020-10-17 00:23:59 -0600

    Casi final falta probar y terminar

--- Proyecto1Lenguajes/main.c
+++ Proyecto1Lenguajes/main.c
@@ -2,57 +2,216 @@
 #include <stdlib.h>
 #include <SFML/Graphics.h>
 #include <SFML/Window.h>
+#include <stdbool.h>
+#include <string.h>
 
 
+//Imagen con tamaño por defecto para realizar re-ajuste.
+struct sfImage* defaultSizeImgMax(){
+    sfImage* resultImg =  sfImage_create(2048,1536);
+    return resultImg;
+}
 
-void testImg(){
-
-    sfImage* img1 = sfImage_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\edificio.jpg");
-
-    sfImage_flipVertically(img1);
-
-    sfTexture* texture = sfTexture_createFromImage(img1, NULL);
-
-    sfImage* testImg =  sfTexture_copyToImage(texture);
-
-    sfFont *font;
-    sfText *text;
-
-    sfVector2u size = sfImage_getSize(testImg);
-
-    font = sfFont_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\Fonts\\noto-sans-bold.ttf");
-    text = sfText_create();
-    sfVector2f vec  = { 0, 0 }; // posicion del texto
-    //sfText_setRotation(text,320);
-    sfText_setPosition(text,vec);
-    sfText_setString(text, "hola");
-    sfText_setFont(text, font);
-    sfText_setCharacterSize(text, 100);
-
-    sfRenderTexture *texturWithText = sfRenderTexture_create(size.x, size.y, sfTrue);
-
-    //sfImage_flipVertically(testImg);
-
-    sfRenderTexture_drawText(texturWithText, text, NULL);
-
-    //sfRenderTexture_display(texturWithText);
-
-    sfTexture* si =  (void*) sfRenderTexture_getTexture(texturWithText);
-
-    sfTexture_updateFromImage(si,img1,size.x, size.y);
-
-    sfImage* im =  sfTexture_copyToImage(si);
-
-    sfImage_flipVertically(im);
-
-    sfImage_saveToFile(im, "C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\edificio2.jpg");
-    printf("\nx= %d y y= %d", size.x, size.y);
+//Imagen con tamaño por defecto para realizar re-ajuste.
+struct sfImage* defaultSizeImg(){
+    sfImage* resultImg =  sfImage_create(1080,720);
+    return resultImg;
 }
 
 
-int main() {
 
-    testImg();
+//Re-ajusta la imagen original a el tamaño predefinido de resizedImg
+void testResize(const sfImage* originalImg, sfImage* resizedImg){
+
+    const sfVector2u sizeOriginalImg = sfImage_getSize(originalImg);
+    const sfVector2u sizeResizedImg = sfImage_getSize(resizedImg);
+
+    for (unsigned int y = 0; y < sizeResizedImg.y; ++y)
+    {
+        for (unsigned int x = 0; x < sizeResizedImg.x; ++x)
+        {
+            unsigned int origX = (unsigned int) ((double) x / (double)sizeResizedImg.x * (double)sizeOriginalImg.x);
+            unsigned int origY = (unsigned int) ((double) y / (double) sizeResizedImg.y * (double) sizeOriginalImg.y);
+            sfImage_setPixel(resizedImg, x,y, sfImage_getPixel(originalImg,origX,origY));
+        }
+    }
+
+}
+
+
+
+//Funcion que verifica las restricciones del tamaño.
+/*
+ * ----Parametros--------
+ * int x : el valor x del tamaño de la imagen
+ * int y : el valor y del tamaño de la imagen
+ * ----Returns--------
+ *  0 : imagen no cumple mínimo
+ *  -1 : imagen excede el máximo, requiere re ajustar el tamañp
+ *  1 : imagen funciona
+*/
+int VerificarSizeImg(unsigned int x, unsigned int y){
+    if (x < 640 && y < 480){
+        return 0;
+    }else if (x > 2048 && y > 1536){
+        return -1;
+    }else
+        return 1;
+}
+
+
+//Funcion que crea la postal:
+/*
+ * ----Parametros--------
+ * char* rutaImg : ruta donde se encuentra la imagen
+ * int extension : valor que especifica la extension, 1 = jpg, 2 = png, 3 = bmp
+*/
+
+void CrearPostal(char* rutaImg, int extension, char* nombreResultado, char* text1, char* text2, int tipoLetra, int tamannoLetra){
+
+    sfImage* img1 = sfImage_createFromFile(rutaImg);
+
+    sfImage_flipVertically(img1);
+
+    sfVector2u sizeImgToResize = sfImage_getSize(img1);
+
+    struct sfImage* ImgDefaultSize;
+
+    if (VerificarSizeImg(sizeImgToResize.x, sizeImgToResize.y) == 0){
+        printf("La imagen no tiene la resolucion minima que se requiere, intente con otra");
+        exit(-1);
+    }
+    else if(VerificarSizeImg(sizeImgToResize.x, sizeImgToResize.y) == -1){
+        printf("La imagen excede el size maximo, re-ajustando la imagen");
+        ImgDefaultSize = defaultSizeImgMax();
+        testResize(img1,ImgDefaultSize);
+    }else{
+        printf("Estandarizando imagen para crear postal");
+        ImgDefaultSize = defaultSizeImg();
+        testResize(img1,ImgDefaultSize);
+    }
+
+    sfTexture* textureResized = sfTexture_createFromImage(ImgDefaultSize, NULL);
+
+    sfImage* testImgResized  =  sfTexture_copyToImage(textureResized);
+
+    sfVector2u sizeImg = sfImage_getSize(testImgResized);
+
+    //Textos
+    sfFont *font;
+    if (tipoLetra == 1){
+        font = sfFont_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\Fonts\\noto-sans-bold.ttf");
+    }
+    else if  (tipoLetra == 2){
+        font = sfFont_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\Fonts\\boxicons.ttf");
+    }
+    else if (tipoLetra == 3){
+        font = sfFont_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\Fonts\\fontawesome-webfont.ttf");
+    }else{
+        printf("El tipo de letra ingresado no existe, colocando tipo de letra default: 1");
+        font = sfFont_createFromFile("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\Fonts\\noto-sans-bold.ttf");
+    }
+    sfText *textArriba;
+
+    sfText *textAbajo;
+
+    int sizeLetra;
+
+    if (tamannoLetra == 1){
+        sizeLetra = 100;
+    }
+    else if  (tamannoLetra == 2){
+        sizeLetra = 75;
+    }
+    else if (tamannoLetra == 3){
+        sizeLetra = 50;
+    }else{
+        printf("El tamanno de letra ingresado no existe, colocando tamanno default: 1");
+        sizeLetra = 100;
+    }
+
+    textArriba = sfText_create();
+    sfVector2f vecArriba  = { 0, 0 }; // posicion del texto
+    sfText_setPosition(textArriba,vecArriba);
+    sfText_setString(textArriba, text1);
+    sfText_setFont(textArriba, font);
+    sfText_setCharacterSize(textArriba, sizeLetra);
+
+    textAbajo = sfText_create();
+    sfVector2f vecAbajo  = { 100, 100 }; // posicion del texto
+    sfText_setPosition(textAbajo,vecAbajo);
+    sfText_setString(textAbajo, text2);
+    sfText_setFont(textAbajo, font);
+    sfText_setCharacterSize(textAbajo, sizeLetra);
+
+
+    sfRenderTexture *texturWithText = sfRenderTexture_create(sizeImg.x, sizeImg.y, sfTrue);
+
+    sfRenderTexture_drawText(texturWithText, textArriba, NULL);
+
+    sfRenderTexture_drawText(texturWithText, textAbajo, NULL);
+
+    sfTexture* renderTexturWithText =  (void*) sfRenderTexture_getTexture(texturWithText);
+
+    sfTexture_updateFromImage(renderTexturWithText, img1, sizeImg.x, sizeImg.y);
+
+    sfImage* resultImg =  sfTexture_copyToImage(renderTexturWithText);
+
+    sfImage_flipVertically(resultImg);
+
+    if (extension == 1){
+        char* str1;
+        char* str2;
+        char* str3;
+        str1 = "C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\";
+        str2 = nombreResultado;
+        str3 = ".jpg";
+        char * str4 = (char *) malloc(1 + strlen(str1)+ strlen(str2) + strlen(str3));
+        strcpy(str4, str1);
+        strcat(str4, str2);
+        strcat(str4, str3);
+        sfImage_saveToFile(resultImg, str4);
+        printf("\nImagen fue generada correctamente con size: x= %d y y= %d", sizeImg.x, sizeImg.y);
+    }
+    else if  (extension == 2){
+        char* str1;
+        char* str2;
+        char* str3;
+        str1 = "C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\";
+        str2 = nombreResultado;
+        str3 = ".png";
+        char * str4 = (char *) malloc(1 + strlen(str1)+ strlen(str2) + strlen(str3));
+        strcpy(str4, str1);
+        strcat(str4, str2);
+        strcat(str4, str3);
+        sfImage_saveToFile(resultImg, str4);
+        printf("\nImagen fue generada correctamente con size: x= %d y y= %d", sizeImg.x, sizeImg.y);
+    }
+    else if (extension == 3){
+        char* str1;
+        char* str2;
+        char* str3;
+        str1 = "C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\";
+        str2 = nombreResultado;
+        str3 = ".bmp";
+        char * str4 = (char *) malloc(1 + strlen(str1)+ strlen(str2) + strlen(str3));
+        strcpy(str4, str1);
+        strcat(str4, str2);
+        strcat(str4, str3);
+        sfImage_saveToFile(resultImg, str4);
+        printf("\nImagen fue generada correctamente con size: x= %d y y= %d", sizeImg.x, sizeImg.y);
+    }else
+        printf("La extension es invalida, intente con otra imagen");
+}
+
+int main() {
+    //Crear postal con imagen por parametro
+    //Postal con formato JPG
+    CrearPostal("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\edificio.jpg",1,"edificio2","sdasdasdasdasdasdasdfasdnas dnas dkas jasnd nasjknd kj","abasdasdasdasjo",1,3);
+    //Postal con formato PNG
+    //CrearPostal("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\edificio2.png");
+    //Postal con formato BMP
+    //CrearPostal("C:\\Users\\Lester Trejos\\Documents\\RepoLenguajesProyecto1\\repolenguajesproyecto1\\Proyecto1Lenguajes\\Assets\\testImgMin.jpg");
 
     return 0;
 }
